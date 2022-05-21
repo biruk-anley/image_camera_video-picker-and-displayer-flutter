c8b999f8ef7ae951f45175d445eb5926253e39b7
biruk <birukanley2022@gmail.com>
2022-05-21 09:33:54 +0300

    remove un necessary fie

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:image_picker/image_picker.dart';
-import 'package:video_player/video_player.dart';
+
 
 void main() {
   runApp(const MyApp());
